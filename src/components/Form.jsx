c42939bf214185084e463bdce7f4e03c0c9f3d7f
Brandon_Hart <sugb73@gmail.com>
2022-12-06 23:15:33 -0500

    Started on the todo inputs

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Form = () => {
+  return (
+    <>
+      <form>
+        <input type='text' className='todo_list_input' />
+        <button className='todo_list_button' type='submit'></button>
+      </form>
+    </>
+  );
+};
+
+export default Form;
