67f73892f9f3a4f9e93d524898fbf6122cd4be69
Brandon_Hart <sugb73@gmail.com>
2022-12-09 16:33:24 -0500

    Small styling changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,20 +10,24 @@ const Form = ({ setUserInput }) => {
 
   return (
     <>
-      <form className='form__style'>
-        <input
-          onChange={userInputHandler}
-          type='text'
-          className='todo_list_input'
-        />
-        <button className='todo_list_button' type='submit'></button>
-        <div className='select'>
-          {/* -------- Select which items to show -------- */}
-          <select className='filter__todo'>
-            <option value='all'>All</option>
-            <option value='complete'>Completed Tasks</option>
-            <option value='incomplete'>Incomplete Tasks</option>
-          </select>
+      <form>
+        <div>
+          <input
+            className='todo_list_input'
+            onChange={userInputHandler}
+            type='text'
+          />
+          <button className='todo_list_button' type='submit'>
+            Input
+          </button>
+          <div className='select__box'>
+            {/* -------- Select which items to show -------- */}
+            <select>
+              <option value='all'>All</option>
+              <option value='complete'>Completed Tasks</option>
+              <option value='incomplete'>Incomplete Tasks</option>
+            </select>
+          </div>
         </div>
       </form>
     </>
