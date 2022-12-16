77cd44fdac7ac8d1cf720879b07aa0f00292b7fe
Brandon_Hart <sugb73@gmail.com>
2022-12-16 01:45:15 -0500

    Added styling to weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,14 +4,16 @@ import './Weather.css';
 function Weather() {
   return (
     <div className='weather__container'>
-      <div className='weather'>
-        <h2>Weather</h2>
-      </div>
-      <div className='location'>
-        <h2>Location</h2>
-      </div>
-      <div className='forecast'>
-        <h2>Forecast</h2>
+      <div className='weather__info'>
+        <div className='location'>
+          <h2>Location</h2>
+        </div>
+        <div className='temperature'>
+          <h1>Weather</h1>
+        </div>
+        <div className='forecast'>
+          <h2>Forecast</h2>
+        </div>
       </div>
     </div>
   );
